910f3b87468c25dc093955a32cea8c240e9f2070
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-12-20 22:52:18 -0700

    Add ResoContainer render test

--- src/mockData.js
+++ src/mockData.js
@@ -5,3 +5,9 @@ export const mockReso = {
   time: '7:00', 
   number: '2'
 }
+
+export const mockReservations = [
+  {id: 1, name: 'Lola', date: '12/21', time: '7:00', number: 2},
+  {id: 2, name: 'Finn', date: '12/22', time: '7:30', number: 3},
+  {id: 3, name: 'Amy', date: '12/25', time: '5:00', number: 5}
+]
