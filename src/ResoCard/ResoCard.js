b8414728f892597bfb6d820721f2d9f59bb8e918
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-16 11:45:46 -0700

    Add styles to resoCard

--- src/ResoCard/ResoCard.js
+++ src/ResoCard/ResoCard.js
@@ -6,9 +6,9 @@ const ResoCard = ({ id, name, date, time, number, removeDeletedReso }) => {
 	return (
 		<div className='reso-card'>
 			<h3>{name}</h3>
-				<p>Date: {date}</p>
-				<p>Time: {time}</p>
-				<p>Number: {number}</p>
+				<p><b>Date:</b> {date}</p>
+				<p><b>Time:</b> {time}</p>
+				<p><b>Number:</b> {number}</p>
 			<button className='delete-btn' onClick={() => removeDeletedReso(id)}>CANCEL</button>
 		</div>
 	)
